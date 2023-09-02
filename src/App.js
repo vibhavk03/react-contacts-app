081445404f341919925175cdaac9cd26e2209d4d
vibhavk03 <vibhavkashyap3@gmail.com>
2023-09-02 16:43:18 +0530

    adding search contacts functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NewContactForm from './components/NewContactForm';
 import ContactList from './components/ContactList';
 
 function App() {
+  /* getting contactList data from local storage */
   const [contactList, setContactList] = useState(() => {
     const localValue = localStorage.getItem('CONTACT_LIST');
     if (localValue == null) {
@@ -12,7 +13,9 @@ function App() {
       return JSON.parse(localValue);
     }
   });
+  const [searchQuery, setSearchQuery] = useState('');
 
+  /* this will update the value in local storage when the contactList data changes */
   useEffect(() => {
     localStorage.setItem('CONTACT_LIST', JSON.stringify(contactList));
   }, [contactList]);
@@ -32,6 +35,16 @@ function App() {
     });
   };
 
+  /* this function will filter the contacts based on the search query */
+  const keys = ['name', 'phone', 'email'];
+  const search = (contactList) => {
+    return contactList.filter((contact) =>
+      keys.some((key) =>
+        contact[key].toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    );
+  };
+
   // console.log('(((((((((((()))))))))))))', contactList);
 
   return (
@@ -39,8 +52,15 @@ function App() {
       <div>
         <h1>Contact App</h1>
       </div>
+      <input
+        type="text"
+        value={searchQuery}
+        placeholder="Search..."
+        className="search-box"
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
       <NewContactForm onSubmit={addContact} />
-      <ContactList contactList={contactList} />
+      <ContactList contactList={search(contactList)} />
     </div>
   );
 }
