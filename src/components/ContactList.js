79c01970348384a2b14937f3cab35bcf4367f79c
vibhavk03 <vibhavkashyap3@gmail.com>
2023-09-02 16:49:54 +0530

    removing waste files

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,17 +1,16 @@
-import ContactTile from './ContactTile';
 import ContactRow from './ContactRow';
 
 export default function ContactList(props) {
   const { contactList } = props;
   return (
     <div className="contact-list-container flex">
-      {/* {contactList.length === 0 && 'No Contacts Added'} */}
       <table>
         <thead>
           <tr>
             <th>🐲 Name</th>
             <th>📞 Phone</th>
             <th>📧 Email</th>
+            <th>🗑️ Delete</th>
           </tr>
         </thead>
         <tbody>
